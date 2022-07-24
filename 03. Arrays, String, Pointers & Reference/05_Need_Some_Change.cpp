b3ba59055a9b97d83ddc581fa59c0b75d07222e1
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-07-24 23:48:53 +0530

    Add files via upload

---
+++ 03. Arrays, String, Pointers & Reference/05_Need_Some_Change.cpp
@@ -0,0 +1,60 @@
+// https://practice.geeksforgeeks.org/problems/need-some-change/1
+
+//{ Driver Code Starts
+//Initial Template for C++
+#include <iostream>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+/*Function to swap array elements
+* arr : array input
+* sizeof_array : number of elements in array
+*/
+class Solution{
+  public:
+    void swapElements(int arr[], int n){
+        
+        for(int i=0;i<n-2;i++)
+        {
+            swap(arr[i],arr[i+2]);
+        }
+        // return arr;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver code to test swapElements
+int main() {
+	
+	int testcase;
+	
+	cin >> testcase;
+	
+	while(testcase--){
+	    int sizeof_array;
+	    cin >> sizeof_array;
+	    
+	    int arr[sizeof_array];
+	    
+	    for(int i = 0;i<sizeof_array;i++){
+	        cin >> arr[i];
+	    }
+        Solution obj;
+        // calling function to swap the array swap elements
+	    obj.swapElements(arr, sizeof_array);
+	    
+	    // Printing the modified array
+	    for(int i = 0;i<sizeof_array;i++){
+            cout << arr[i] << " ";
+        }
+	    
+	    cout << endl;
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
