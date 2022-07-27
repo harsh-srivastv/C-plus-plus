43a5d1e867e8c954c722d8db41099b7a4afa5474
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-07-27 13:57:34 +0530

    Add files via upload

---
+++ 03. Arrays, String, Pointers & Reference/15_Logical Operators.cpp
@@ -0,0 +1,37 @@
+// https://practice.geeksforgeeks.org/problems/logical-operators/1?page=1&category[]=cpp-operator&sortBy=submissions
+
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void logicOp(int a, int b);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+
+void logicOp(int a, int b)
+{
+    /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces*/ 
+    cout<<(a&&b)<<" "<<(a||b)<<" "<<((!a)&&(!b));
+    cout<<endl;
+}
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    logicOp(a,b);
+	    
+	}
+	return 0;
+}
+// } Driver Code Ends
