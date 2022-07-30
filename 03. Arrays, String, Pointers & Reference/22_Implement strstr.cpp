a3a77a4277d562088e9c282b9fcbf8dccaf4705a
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-07-30 11:24:42 +0530

    Add files via upload

---
+++ 03. Arrays, String, Pointers & Reference/22_Implement strstr.cpp
@@ -0,0 +1,34 @@
+// https://practice.geeksforgeeks.org/problems/implement-strstr/1
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int strstr(string ,string);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a;
+        string b;
+        
+        cin>>a;
+        cin>>b;
+        
+        cout<<strstr(a,b)<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+//Function to locate the occurrence of the string x in the string s.
+int strstr(string s, string x)
+{
+     //Your code here
+     int pos = s.find(x);
+     return pos;
+}
