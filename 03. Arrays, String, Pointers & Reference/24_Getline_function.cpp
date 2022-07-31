e2ba60a89eef458f3f6615b76c1bee47669fa2b8
Harsh Srivastava <88231697+harsh-srivastv@users.noreply.github.com>
2022-07-31 22:31:46 +0530

    Add files via upload

---
+++ 03. Arrays, String, Pointers & Reference/24_Getline_function.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{   
+    string str;
+    cout<<"Enter your Name: ";
+    getline(cin, str);
+    cout<<"Your name is: "<<str;
+    return 0;
+}
